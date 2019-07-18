2a192010a59d4241bfd305c001ed68dff3fc5b7d
abdorihan <abdorihan@yahoo.com>
2019-07-18 10:29:12 +0200

    Update data.js

--- server/router/test/data.js
+++ server/router/test/data.js
@@ -1,6 +1,20 @@
+const { Client } = require('pg');
+const client = new Client({
+  user: 'qauanlkaanhjva',
+  host: 'ec2-54-247-170-5.eu-west-1.compute.amazonaws.com',
+  database: 'dcg0j61qf4kvpi',
+  password: '34bf818e7847ec05e50e39dae675d19c10e23632304e5e32a9e40d669ea7326b',
+  port: 5432,
+});
+
 export async function getData () {
     const result = { ok: true};
-    return Promise.resolve(result);
+    await client.connect();
+
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message); // Hello world!
+    await client.end();
+    return Promise.resolve(res.rows[0].message);
 }
 
 export default { getData};
