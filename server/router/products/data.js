43820e380741a8193a5b3d7537df4e01c0fd489b
Abduladim Rehan <abdorihan@yahoo.com>
2021-02-28 13:47:42 +0200

    sell

--- server/router/products/data.js
+++ server/router/products/data.js
@@ -23,6 +23,7 @@ export async function getData () {
     database: 'd8ccpl1b2tjrpn',
     password: 'ed8bd878cdf38e747b8eca3209095b0888ff0b7243806185fb8cc1f311cc97b2',
     port: 5432,
+    ssl: { rejectUnauthorized: false }
   };
     const result = { ok: true};
     console.log('before db');
